19d619eca26a60e330c8cd2c2dcaeff3039c1155
meomeomeo2004 <nquchien3004@gmail.com>
2026-03-07 17:11:32 +0700

    Login & Register function

---
+++ src/main/java/com/example/spotify/SpotifyCloneBackendApplication.java
@@ -0,0 +1,17 @@
+package com.example.spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class SpotifyCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCloneBackendApplication.class, args);
+
+
+
+	}
+
+}
---
+++ src/main/java/com/example/spotify/entity/User.java
@@ -0,0 +1,55 @@
+package com.example.spotify.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import com.example.spotify.entity.Role;
+import com.example.spotify.entity.Status;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "is_premium")
+    private Boolean isPremium;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
